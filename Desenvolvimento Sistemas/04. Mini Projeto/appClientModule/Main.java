a430cbd6d094b702e0776d06c43b166b50482b77
josuesantos7 <josuesantossantosvf@gmail.com>
2026-06-18 21:57:50 -0300

    add metodo mostrarHoras e BuscarPorPlaca

--- Desenvolvimento Sistemas/04. Mini Projeto/appClientModule/Main.java
+++ Desenvolvimento Sistemas/04. Mini Projeto/appClientModule/Main.java
@@ -1,7 +1,57 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+import Model.Veiculo;
+import dao.VeiculoDAO;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("TESTE");
+		Scanner scanner = new Scanner(System.in);
+		VeiculoDAO veiculodao = new VeiculoDAO();
+		
+		System.out.println("***** Bem vindo ao estacionamento JS *****");
+		int opcao = 0;
+		
+		while (opcao != 3) {
+			System.out.println("\n========= MENU ========");
+			System.out.println("1 - Emitir ticket Entrada");
+			System.out.println("2 - Validar ticket Saída");
+			System.out.println("3 - finalizar Programa");
+			
+			opcao = Integer.parseInt(scanner.nextLine());
+			
+			LocalDateTime tempoAtual;
+			switch (opcao) {
+			case 1:
+				System.out.println("Informe a placa do seu veículo: ");
+				String placa = scanner.nextLine();
+				System.out.println(placa);
+				
+				tempoAtual= LocalDateTime.now();
+				
+				Veiculo veiculo = new Veiculo(placa, tempoAtual);
+				
+				veiculodao.inserir(veiculo);
+				break;
+			case 2:
+				System.out.println("Informe a placa do seu veículo: ");
+				String placaSaida = scanner.nextLine();
+				System.out.println(placaSaida);
+				
+				tempoAtual= LocalDateTime.now();
+				
+				// veiculodao.mostrarHoras(placaSaida);
+				veiculodao.buscarPorPlaca(placaSaida);
+				
+				break;
+				
+				
+
+			default:
+				break;
+			}
+		}
 	}
 
 	/* (non-Java-doc)
--- Desenvolvimento Sistemas/04. Mini Projeto/appClientModule/dao/VeiculoDAO.java
+++ Desenvolvimento Sistemas/04. Mini Projeto/appClientModule/dao/VeiculoDAO.java
@@ -24,16 +24,68 @@ public class VeiculoDAO {
 			
 			stmt.execute();
 			ResultSet rs = stmt.getGeneratedKeys();
-			/*
-			if(rs.next()) {
-				cliente.setId(rs.getInt(1));
-			}
-			*/
+			
 			System.out.println("Veículo cadastrado com sucesso!");
 			
 		} catch (Exception e) {
 			// TODO: handle exception
 			throw new RuntimeException("Erro ao cadastrar o veículo", e);
 		}
+	}
+	
+	public void mostrarHoras(String placa) {
+		String sql = "select placa, entrada, saida from Veiculo where placa = ?";
+		
+		try (
+				Connection conn = ConectFactory.getConnection();
+				PreparedStatement stmt = conn.prepareStatement(sql);
+				) {
+			
+			stmt.setString(1, placa);
+			
+
+			ResultSet rs = stmt.executeQuery();
+			
+			    while (rs.next()) {
+			        Object entrada = rs.getObject("entrada");
+			        Object saida = rs.getObject("saida");
+			
+			        System.out.println("Placa: " + placa);
+			        System.out.println("Entrada: " + entrada);
+			        System.out.println("Saída: " + saida);
+			    }
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			throw new RuntimeException("Erro ao cadastrar o veículo", e);
+		}
+	}
+	
+	public Veiculo buscarPorPlaca(String placa) {
+		String sql = "SELECT PLACA, entrada, saida from Veiculo WHERE PLACA = ?";
+		
+		try (
+				Connection conn = ConectFactory.getConnection();
+				PreparedStatement stmt = conn.prepareStatement(sql);
+				){
+			stmt.setString(1, placa);
+			ResultSet rs = stmt.executeQuery();
+			
+			if(rs.next()) {
+				Veiculo veiculo = new Veiculo(
+						rs.getString("placa"),
+						rs.getTimestamp("entrada").toLocalDateTime()
+				);
+				veiculo.setSaida(LocalDateTime.now());
+				//veiculo.setSaida(rs.getTimestamp("saida").toLocalDateTime());
+				
+				System.out.println("a placa é: " + veiculo.getPlaca());
+				return veiculo ;		
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("Erro ao buscar veiculo", e);
+		}
+		
+		return null;
 	}
 }
